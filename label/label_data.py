fe13bb8b346223e65f5fcb9801e0e865c98599b6
rwarren2163 <richardwarren2163@gmail.com>
2021-01-10 17:34:12 -0500

    can now read mis-labeled frames from spreadsheet

--- label/label_data.py
+++ label/label_data.py
@@ -1,22 +1,32 @@
-from deepposekit import Annotator
+import argparse
+import yaml
 import sys
+import os
 
-# get arguments
-dataset, skeleton = sys.argv[1:3]
-if len(sys.argv)==4:  # optional third arg for image brightness
-	brightness = float(sys.argv[3])
-else:
-	brightness = 1.0
 
-print(f'dataset: {dataset}')
-print(f'skeleton: {skeleton}')
+# get args
+parser = argparse.ArgumentParser()
+parser.add_argument('-vid_type', type=str, default="free", help="free or fixed, depending on dataset type")
+parser.add_argument('-root_dir', type=str, default=r"D:\\github\\fish-tracking\\", help="fish-tracking github repo location")
+parser.add_argument('-brightness', type=float, default=1.0, help='adjust brightness by positive scalar multiple')
+args = parser.parse_args()
 
-app = Annotator(datapath=dataset,
+
+# load config
+with open(os.path.join(args.root_dir, 'config.yaml')) as f:
+	cfg = yaml.safe_load(f)[args.vid_type]
+
+
+print('dataset: ', cfg['dataset'])
+print('skeleton:', cfg['skeleton'])
+
+from deepposekit import Annotator
+app = Annotator(datapath=cfg['dataset'],
                 dataset='images',
-                skeleton=skeleton,
+                skeleton=cfg['skeleton'],
                 shuffle_colors=True,
                 text_scale=0.2,
                 scale=1.0,
-                brightness=brightness)
+                brightness=args.brightness)
 
 app.run()
